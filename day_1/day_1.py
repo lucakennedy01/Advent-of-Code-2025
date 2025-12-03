f4d43e34fa8e397c236ff90ecbfac9c09a9d6a4d
Luca Kennedy <luca.kennedy@phmg.com>
2025-12-03 14:10:36 +0000

    Added day 1 part 2

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -17,4 +17,25 @@ with open("day_1/input.csv") as f:
         if dial == 0:
             zeros += 1
 
-print("zeros:", zeros)
+print("part 1 zeros:", zeros)
+zeros = 0
+dial = 50
+
+with open("day_1/input.csv") as f:
+    
+    for x in f:
+        x = x.strip()
+        direction = x[0]
+        value = int(x[1:])
+
+        # Incremental approach
+        step = 1 if direction == "R" else -1
+
+        for _ in range(value):
+            dial = (dial + step) % 100
+            if dial == 0:
+                zeros += 1
+
+       
+
+print(f"part 2 zeros: {zeros}")
